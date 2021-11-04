94c6060df3afad630c521cf124603e01101f3e57
korkmazfb <32840@ma-web.nl>
2021-11-04 10:07:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let btneerste = document.querySelector('#eerste');
+let btntweede = document.querySelector('#tweede');
+let btnderde = document.querySelector('#derde');
+
+btneerste.addEventListener('click', ()=>
+{
+    
+    btneerste.style.backgroundColor= '#337ab7';}
+ )
+btntweede.addEventListener('click', ()=> 
+
+{
+    
+    btntweede.style.backgroundColor= '#93020c';}
+
+    )
+btnderde.addEventListener('click', ()=>
+
+{
+    btnderde.style.backgroundColor= '#4cae4c'}
+    )
